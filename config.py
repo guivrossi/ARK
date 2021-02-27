7d80dbe5873cc59630c5c36faed51a0309b56e15
Guilherme Rossi <31310677+guivrossi@users.noreply.github.com>
2021-02-27 15:16:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 API_URL = "https://paper-api.alpaca.markets"
-API_KEY = "PKXVK11TOQKJ0FJRCFOM"
-API_SECRET = "VdsxWqzbheySof5gcTEJsLMiiz6XmZ9gXgauPwOX"
+API_KEY = "your key"
+API_SECRET = "your secret"
 
 DB_HOST = 'localhost'
 DB_USER = 'postgres'
-DB_PASS = 'password'
+DB_PASS = 'your password'
 DB_NAME = 'etfdb'
